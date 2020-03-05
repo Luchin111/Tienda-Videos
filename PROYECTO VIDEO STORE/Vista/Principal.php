44dfc8b54367ba14fbc7d67b7b834ca9b839def3
unknown <luisinmedina@gmail.com>
2020-03-05 13:53:25 -0400

    Change Style Principal class

--- PROYECTO VIDEO STORE/Vista/Principal.php
+++ PROYECTO VIDEO STORE/Vista/Principal.php
@@ -14,7 +14,7 @@
   
   
 <head>
-    <title>Teleferico</title>
+    <title>VIDEO STORE</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimun-scale=1">
     <link rel="stylesheet" href="css/fontelloPrincipal.css">
@@ -38,11 +38,11 @@
      
         <main>
             <section id="banner">
-                <img src="img/banner.jpg">
+                <img src="img/banner-video3.jpg">
                 <div class="contenedor">
                     <h2>VIDEO STORE</h2>
                     <p>Plataforma Beta</p>
-                    <a href="" target="_blank">Nuevo Usuario</a>
+                    <a href="Registro.php" target="_blank">Nuevo Usuario</a>
                 </div>
             </section>
             
@@ -54,19 +54,59 @@
             </section>
             
             <section id="blog">
+                <h3>Videos</h3>
+                <div class="contenedor">
+                    <article>
+                        <a href="recargas.php"><img src="img/añadir.jpg" title="Añadir Video" alt=""></a>
+                        <h4>Añadir Video</h4>
+                    </article>
+                    <article>
+                        <img src="img/añadir-copia.jpg" title="Registrar Copia" alt="">
+                        <h4>Registrar nueva Copia</h4>
+                    </article>
+                    <article>
+                        <a href="Precios.php"><img  src="img/dar-de-baja.jpg" title="Dar de baja una copia" alt=""></a>
+                        <h4>Registrar Bajas</h4>
+                    </article>
+                </div>
+            </section>
+
+            <section id="blog">
+                <h3>Clientes</h3>
+                <div class="contenedor">
+                    <article>
+                        <a href="recargas.php"><img src="img/add-user.jpg" title="Registrar Cliente" alt=""></a>
+                        <h4>Registrar Cliente</h4>
+                    </article>
+                    <article>
+                        <img src="img/users.png" title="Ver Clientes" alt="">
+                        <h4>Ver Clientes</h4>
+                    </article>
+                    <article>
+                        <a href="Precios.php"><img  src="img/bloquear-usuario.jpg" title="Bloquear Cliente" alt=""></a>
+                        <h4>Bloquear Cliente</h4>
+                    </article>
+                </div>
+            </section>
+
+            <section id="blog">
                 <h3>Operaciones</h3>
                 <div class="contenedor">
                     <article>
-                        <a href="recargas.php"><img src="img/contactanos.jpg" title="Ir a Recargas" alt=""></a>
-                        <h4>Recarga</h4>
+                        <a href="recargas.php"><img src="img/prestamo.png" title="Nueva Renta" alt=""></a>
+                        <h4>Registrar Renta</h4>
                     </article>
                     <article>
-                        <img src="img/reportes.png" title="Ir a reportes" alt="">
-                        <h4>Reportes</h4>
+                        <img src="img/prestamos-realizados.png" title="Ver Rentas" alt="">
+                        <h4>Ver Prestamos</h4>
                     </article>
                     <article>
-                        <a href="Precios.php"><img  src="img/cajero.png" title="Fijar Costos" alt=""></a>
-                        <h4>Fijar Costos</h4>
+                        <img src="img/cajero.png" title="Costos por DIA" alt="">
+                        <h4>Modificar Costos</h4>
+                    </article>
+                    <article>
+                        <a href="descuentos.php"><img  src="img/descuentos.png" title="Descuento de Peliculas" alt=""></a>
+                        <h4>Modificar Descuentos</h4>
                     </article>
                 </div>
             </section>
@@ -75,19 +115,19 @@
                 <h3>Los videos son el futuro.</h3>
                 <div class="contenedor">
                     <div class="info-pet">
-                        <img src="img/1.jpg" alt="">
+                        <img src="img/relaja.jpg" alt="">
                         <h4>Relaja</h4>
                     </div>
                     <div class="info-pet">
-                        <img src="img/2.jpg" alt="">
+                        <img src="img/motivar.jpg" alt="">
                         <h4>Motiva</h4>
                     </div>
                     <div class="info-pet">
-                        <img src="img/3.jpg" alt="">
+                        <img src="img/reir.jpg" alt="">
                         <h4>Hace reir</h4>
                     </div>
                     <div class="info-pet">
-                        <img src="img/4.jpg" alt="">
+                        <img src="img/llorar.jpg" alt="">
                         <h4>Hace llorar</h4>
                     </div>
                 </div>
@@ -97,7 +137,7 @@
      
         <footer>
             <div class="contenedor">
-                <p class="copy">Luis Medina &copy; 2019</p>
+                <p class="copy">Luis Medina &copy; 2020</p>
                 <div class="sociales">
                     <a class="fontawesome-facebook-sign" href="#"></a>
                     <a class="fontawesome-twitter" href="#"></a>
